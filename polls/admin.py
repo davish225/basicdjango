ca6b22383ef49828ebf53929785942181319df59
Davis <davish225@gmail.com>
2020-08-26 12:24:47 -0400

    added questions and choices to admin area

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Question, Choice
+
+
+admin.site.site_header = "Poll Admin"
+admin.site.site_title = "Poll Admin area"
+admin.site.index_title = "Welcome to the Poll Admin area"
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields':['question_text']}), ('Date information', {'fields': ['pub_date'], 'classes':['collapse']}), ]
+    inlines = [ChoiceInLine]
+
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
