d823913c174faec9b60ad53a68060491e5a3e371
Davis <davish225@gmail.com>
2020-08-26 23:04:52 -0400

    added detail and result buttons

--- polls/views.py
+++ polls/views.py
@@ -7,4 +7,21 @@ from .models import Question, Choice
 
 
 def index(request):
-    return render(request, 'polls/index.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+# Show specific questiona nd choices
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/results.html', {'question': question})
+
+
+# Get question and display results
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
